ea40d2013739ab04d635e9a1116ab78f88a363ac
muratilgun <muratilgun34@gmail.com>
2022-07-01 00:12:09 +0300

    Self-configuring Network Monitor

---
+++ ReflectionSampleConsole/NetworkMonitorClasses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReflectionSampleConsole
+{
+    public class MailService
+    {
+        public void SendMail(string address, string subject)
+        {
+            Console.WriteLine($"Sending a warning mail to {address} with subject {subject}.");
+        }
+    }
+
+    public class SoundHornService
+    {
+        public void SoundHorn(string volume)
+        {
+            Console.WriteLine($"Making noise with the volume turned up to {volume}.");
+        }
+    }
+
+    public class NetworkMonitorSettings
+    {
+        public string WarningService { get; set; }
+        public string MethodToExecute { get; set; }
+        public Dictionary<string, object> PropertyBag { get; set; }= new Dictionary<string, object>(
+            StringComparer.OrdinalIgnoreCase);
+    }
+}
--- ReflectionSampleConsole/Program.cs
+++ ReflectionSampleConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace ReflectionSampleConsole
@@ -6,9 +8,90 @@ namespace ReflectionSampleConsole
     internal class Program
     {
         private static string _typeFromConfiguration = "ReflectionSampleConsole.Person";
+        private static NetworkMonitorSettings _networkMonitorSettings = new NetworkMonitorSettings();
+        private static Type _warningServiceType;
+        private static MethodInfo _warningServiceMethod;
+        private static object _warningService;
+        private static List<object> _warningServiceParameterValues;
         static void Main(string[] args)
         {
+            BootStrapFromConfiguration();
 
+            Console.WriteLine("Monitoring network... something went wrong.");
+            Warn();
+
+            Console.ReadLine();
+        }
+        private static void Warn()
+        {
+            // first, create an instance of the service if it wasn't cached yet
+            if (_warningService == null)
+            {
+                _warningService = Activator.CreateInstance(_warningServiceType);
+            }
+
+            // then, call the method on it, passing through the property bag 
+            // create a list of parameters
+            var parameters = new List<object>();
+            foreach (var propertyBagItem in _networkMonitorSettings.PropertyBag)
+            {
+                parameters.Add(propertyBagItem.Value);
+            }
+
+            _warningServiceMethod.Invoke(_warningService, parameters.ToArray());
+        }
+        private static void BootStrapFromConfiguration()
+        {
+            var appSettingsConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            appSettingsConfig.Bind("NetworkMonitorSettings", _networkMonitorSettings);
+            // inspect the assembly to check whether the correct types are contained within
+            _warningServiceType = Assembly.GetExecutingAssembly()
+                .GetType(_networkMonitorSettings.WarningService);
+            if (_warningServiceType == null)
+            {
+                throw new Exception("Configuration is invalid - warning service not found");
+            }
+
+            // inspect the service for the required method 
+            _warningServiceMethod = _warningServiceType
+                .GetMethod(_networkMonitorSettings.MethodToExecute);
+            if (_warningServiceMethod == null)
+            {
+                throw new Exception("Configuration is invalid - method to execute " +
+                                    "on warning service not found");
+            }
+
+            // check if the parameters match
+            foreach (var parameterInfo in _warningServiceMethod.GetParameters())
+            {
+                if (!_networkMonitorSettings.PropertyBag.TryGetValue(
+                        parameterInfo.Name, out object parameterValue))
+                {
+                    // parameter name cannot be found
+                    throw new Exception($"Configuration is invalid - parameter " +
+                                        $"{parameterInfo.Name} not found.");
+                };
+
+                _warningServiceParameterValues = new List<object>();
+                try
+                {
+                    var typedValue = Convert.ChangeType(
+                        parameterValue, parameterInfo.ParameterType);
+                    _warningServiceParameterValues.Add(typedValue);
+                }
+                catch
+                {
+                    throw new Exception($"Configuration is invalid - parameter {parameterInfo.Name} " +
+                                        $"cannot be converted to expected type {parameterInfo.ParameterType}.");
+                }
+            }
+        }
+
+        public void InstantiatingAndManipulatingObjects()
+        {
 
             var personType = typeof(Person);
             // Private Constructor
@@ -74,18 +157,18 @@ namespace ReflectionSampleConsole
                 null,
                 null).Unwrap();
 
-            var nameProperty =  personForManipulation.GetType().GetProperty("Name");
+            var nameProperty = personForManipulation.GetType().GetProperty("Name");
             nameProperty.SetValue(personForManipulation, "Sven");
 
             var ageField = personForManipulation.GetType().GetField("age");
-            ageField.SetValue(personForManipulation,23);
+            ageField.SetValue(personForManipulation, 23);
 
             var privateField = personForManipulation.GetType()
                 .GetField("_aPrivateField", BindingFlags.Instance | BindingFlags.NonPublic);
             privateField.SetValue(personForManipulation, "updated private field value");
 
 
-            personForManipulation.GetType().InvokeMember("Name", BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty,null,personForManipulation,new []{"Niyazi"});
+            personForManipulation.GetType().InvokeMember("Name", BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty, null, personForManipulation, new[] { "Niyazi" });
 
             personForManipulation.GetType().InvokeMember("_aPrivateField",
                 BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.SetField,
@@ -100,9 +183,7 @@ namespace ReflectionSampleConsole
                 BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod,
                 null, personForManipulation, new[] { "something to yell" });
 
-            Console.ReadLine();
         }
-
         public void InspectingMetadata()
         {
             string name = "Murat";
